fb8d74ab3e6d73a1414a882b37d8b1e3359abef5
natanepirisso <natanepirisso@gmail.com>
2024-12-16 23:54:41 -0300

    feat: exemplo de animação

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,88 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets, Sprite, Spritesheet, AnimatedSprite } from "pixi.js";
 
 (async () => {
   // Create a new application
   const app = new Application();
-
+  await app.init({ background: 'green', resizeTo: window });
+  document.body.appendChild(app.canvas);
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
-
+  const atlasData= {
+    frames:{
+      idle1:{
+        frame:{x:0, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle2:{
+        frame:{x:78, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle3:{
+        frame:{x:156, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle4:{
+        frame:{x:234, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle5:{
+        frame:{x:312, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle6:{
+        frame:{x:390, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle7:{
+        frame:{x:468, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle8:{
+        frame:{x:546, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle9:{
+        frame:{x:624, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle10:{
+        frame:{x:702, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58}
+      },
+      idle11:{
+        frame:{x:780, y:0, w:78, h:58},
+        sourceSize:{w:78, h:58},
+        spriteSourceSize:{x:0,y:0,w:78,h:58},
+      }
+    },
+    meta:{
+      image:'/public/assets/idle.png',
+      size: {w:858, h:58},
+      scale:.5
+    },
+    animations:{
+      idleHuman:['idle1','idle2','idle3','idle4','idle5','idle6','idle7','idle8','idle9','idle10','idle11']
+    }
+  }
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
-
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const texture = await Assets.load(atlasData.meta.image);
 
   // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  const spritesheet = new Spritesheet(texture, atlasData);
+  await spritesheet.parse();
+  const animatedSprite = new AnimatedSprite(spritesheet.animations.idleHuman);
+  app.stage.addChild(animatedSprite);
+  animatedSprite.play();
+  animatedSprite.animationSpeed= 0.15;
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
 })();
